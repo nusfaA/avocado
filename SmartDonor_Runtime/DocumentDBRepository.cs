9099328c58db812972fbb5f5c3f47c60fda4b150
nusfaA <35840868+nusfaA@users.noreply.github.com>
2018-02-19 13:24:58 +0530

    azure cosmos db intgration

---
+++ SmartDonor_Runtime/DocumentDBRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.Documents; 
+ using Microsoft.Azure.Documents.Client; 
+ using Microsoft.Azure.Documents.Linq; 
+ using System.Configuration;
+ using System.Linq.Expressions;
+ using System.Threading.Tasks;
+using System.Net;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
